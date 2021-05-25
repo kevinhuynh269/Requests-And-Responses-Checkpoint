06900e91ca92d2420acd87da2c182b322d0e987f
kevinhuynh269 <kevinhuynh269@yahoo.com>
2021-05-25 16:07:24 -0500

    First Commit

---
+++ Requests-and-Responses/Controllers/RequestsAndResponsesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Requests_and_Responses.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RequestsAndResponsesController : ControllerBase
+    {
+        // GET
+        public RequestsAndResponsesController()
+        {
+            
+        }
+        [HttpGet("RequestHeaders")]
+        public IActionResult Get()
+        {
+            return Ok(base.Request.Headers);
+        }
+        
+        [HttpGet("ListOfQueryNamesAndValues")]
+        public IActionResult ListOfQueryNamesAndValues(int val1, int val2)
+        {
+            return Ok(base.Request.Query);
+        }
+        
+    }
+}
